becb8f9addfba82d1b6a357f02c57bd5061ad6ee
pglen <peterglen99@gmail.com>
2026-01-04 03:50:10 +0700

    autocheck

--- pip-build.py
+++ pip-build.py
@@ -1,4 +1,4 @@
 rm -fr dist/*
 python3 -m build
-auditwheel repair dist/bluepyenc-1.1.0-cp310-cp310-linux_x86_64.whl -w dist
-rm dist/bluepyenc-1.1.0-cp310-cp310-linux_x86_64.whl
+auditwheel repair dist/bluepyenc-*-cp310-cp310-linux_x86_64.whl -w dist
+rm dist/bluepyenc-*-cp310-cp310-linux_x86_64.whl
